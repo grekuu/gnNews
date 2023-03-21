d3bac6df5f01e51d3b89f9e7391acde8762a73fc
grekuu <gracjanprusik@gmail.com>
2023-03-21 20:49:40 +0100

    api key is hidden now

--- frontend/src/common/newsApi.ts
+++ frontend/src/common/newsApi.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { APIKey } from "./newsApiKey";
 
 export default axios.create({
-  baseURL:
-    "https://newsapi.org/v2/top-headlines?country=pl&apiKey=5be44a79b15a4c10941c670b9765972e",
+  baseURL: `https://newsapi.org/v2/top-headlines?country=pl`,
+  headers: {
+    Authorization: APIKey,
+  },
 });
---
+++ frontend/src/common/newsApiKey.ts
@@ -0,0 +1,1 @@
+export const APIKey = import.meta.env.VITE_REACT_APP_API_KEY;
